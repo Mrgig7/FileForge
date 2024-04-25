caed12fc1a6f14ca810d85820066c13e7d9cb31f
Nitesh J <jnitesh1463@gmail.com>
2024-04-25 09:54:16 +0530

    updated commit

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const cors = require('cors');
 
 app.use(express.static('public'));
 app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
 
+//Cors
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 //Template engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
