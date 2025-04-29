db80f10e00735bea1a1975e1d661f815ca5407e0
Nitesh J <jnitesh1463@gmail.com>
2025-04-30 02:48:48 +0530

    Add file model for database

---
+++ backend/models/file.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const fileSchema = new mongoose.Schema({
+  filename: {
+    type: String,
+    required: true
+  },
+  originalName: {
+    type: String,
+    required: true
+  },
+  path: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: Number,
+    required: true
+  },
+  uuid: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('File', fileSchema);
